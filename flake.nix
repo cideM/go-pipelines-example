277366811ea4abb2562558b9f8ab0f7e4607efe6
Florian Beeres <yuuki@protonmail.com>
2021-07-12 12:02:34 +0200

    initial

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Demonstrating The Pipelines Pattern In Golang";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.go
+            pkgs.gotools
+            pkgs.golangci-lint
+            pkgs.gopls
+          ];
+        };
+      });
+}
